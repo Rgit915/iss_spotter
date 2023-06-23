f1e06a91b68a6b910651955109ea426ceefc7fdc
Rora Alem <rora.goitom@gmail.com>
2023-06-23 10:04:46 -0400

    implement third API call to fetch ISS flyovers

--- iss.js
+++ iss.js
@@ -61,5 +61,34 @@ const fetchCoordsByIP = function(ip, callback) {
 };
 
 
+//ISS Spotter III
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`,(error, response, body) => {
+    //handle errors and check the respose status code
+    if (error) {
+      callback(error, null);
+      return;
+    } else {
+      try {
+        const data = JSON.parse(body);
+        callback(null, data.response);
+      } catch (parseError) {
+        callback(parseError, null);
+      }
+    }
+     
+  });
+};
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
